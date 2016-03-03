f05f780b2f516c5f5785ab41dc097b207041206c
Ricardo Gonzalez <rickgc@gmail.com>
2016-03-03 16:20:36 +0000

    Fix map scroll

--- js/main.js
+++ js/main.js
@@ -152,7 +152,7 @@ jQuery(function($) {
 			bottomMargin = parseInt($dialog.css('marginBottom'), 10);
 
 		// Make sure you don't hide the top part of the modal w/ a negative margin
-		// if it's longer than the screen height, and keep the margin equal to 
+		// if it's longer than the screen height, and keep the margin equal to
 		// the bottom margin of the modal
 		if (offset < bottomMargin) offset = bottomMargin;
 		$dialog.css("margin-top", offset);
@@ -168,4 +168,35 @@ jQuery(function($) {
 	$(window).on("resize", function() {
 		$('.modal:visible').each(centerModal);
 	});
+
+// Disable scroll zooming and bind back the click event
+var onMapMouseleaveHandler = function (event) {
+  var that = $(this);
+
+  that.on('click', onMapClickHandler);
+  that.off('mouseleave', onMapMouseleaveHandler);
+  that.find('iframe').css("pointer-events", "none");
+}
+
+var onMapClickHandler = function (event) {
+  var that = $(this);
+
+  console.log('click!');
+
+  // Disable the click handler until the user leaves the map area
+  that.off('click', onMapClickHandler);
+
+  // Enable scrolling zoom
+  that.find('iframe').css("pointer-events", "auto");
+
+  // Handle the mouse leave event
+  that.on('mouseleave', onMapMouseleaveHandler);
+}
+
+// Enable map zooming with mouse scroll when the user clicks the map
+$('#map').on('click', onMapClickHandler);
+console.log('hey');
+
+
+
 });
